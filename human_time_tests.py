6d28535d7cb7318c37a72d90d689a07232719ad1
Teifion <sarkalian@gmail.com>
2013-12-22 00:33:09 +0000

    Initial upload
    It works but I'm sure there's plenty more to add.

---
+++ human_time_tests.py
@@ -0,0 +1,106 @@
+"Written by Tefion Jordan"
+
+import human_time
+import unittest
+from datetime import datetime
+
+class HTTester(unittest.TestCase):
+    def test_parse(self):
+        vals = (
+            ("every tuesday", (
+                datetime(2013, 12, 10),
+                datetime(2013, 12, 17),
+                datetime(2013, 12, 24),
+            )),
+            
+            ("every weekday", (
+                datetime(2013, 12, 5),
+                datetime(2013, 12, 6),
+                datetime(2013, 12, 9),
+            )),
+            
+            ("every day", (
+                datetime(2013, 12, 5),
+                datetime(2013, 12, 6),
+                datetime(2013, 12, 7),
+            )),
+            
+            ("weekday at noon", (
+                datetime(2013, 12, 5, 12),
+                datetime(2013, 12, 6, 12),
+                datetime(2013, 12, 9, 12),
+            )),
+            
+            ("every tuesday at 4:20am", (
+                datetime(2013, 12, 10, 4, 20),
+                datetime(2013, 12, 17, 4, 20),
+                datetime(2013, 12, 24, 4, 20),
+            )),
+            
+            ("every weekday at 1630", (
+                datetime(2013, 12, 5, 16, 30),
+                datetime(2013, 12, 6, 16, 30),
+                datetime(2013, 12, 9, 16, 30),
+            )),
+            
+            ("first monday of every month", (
+                datetime(2014, 1, 6),
+                datetime(2014, 2, 3),
+                datetime(2014, 3, 3),
+            )),
+            
+            ("second wednesday of every month", (
+                datetime(2013, 12, 11),
+                datetime(2014, 1, 8),
+                datetime(2014, 2, 12),
+            )),
+
+            ("last Friday of every month at 9am", (
+                datetime(2013, 12, 27, 9),
+                datetime(2014, 1, 31, 9),
+                datetime(2014, 2, 28, 9),
+            )),
+            
+            ("15th of every month", (
+                datetime(2013, 12, 15),
+                datetime(2014, 1, 15),
+                datetime(2014, 2, 15),
+            )),
+            
+            ("21 of every month at 1430", (
+                datetime(2013, 12, 21, 14, 30),
+                datetime(2014, 1, 21, 14, 30),
+                datetime(2014, 2, 21, 14, 30),
+            )),
+        )
+        
+        """
+        A calendar of December 2013 (the month this date falls into)
+        for your reference.
+    
+           December 2013
+        Su Mo Tu We Th Fr Sa
+         1  2  3  4  5  6  7
+         8  9 10 11 12 13 14
+        15 16 17 18 19 20 21
+        22 23 24 25 26 27 28
+        29 30 31
+        """
+        start_time = datetime(
+            year = 2013,
+            month = 12,
+            day = 4,
+            hour = 6,
+            minute = 20,
+            second = 5,
+        )
+        
+        for str_in, expected in vals:
+            gen = human_time.parse(str_in, start_time=start_time)
+            
+            for e in expected:
+                r = gen.__next__()
+                self.assertEqual(e, r)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ readme.md
@@ -0,0 +1,54 @@
+Human time
+==========
+The purpose of this library is to take human written time such as "every other tuesday", "every weekday",
+"every friday at 2pm" etc, and convert it into a sequence of datetimes meeting the required parameters.
+The sequence is represented as a generator without a temination so it must be treated as an infinite list.
+
+Main functions is parse(), it returns a generator supplying the sequence
+
+  Some example strings:
+Tuesday
+Every other wednesday
+Weekday
+First friday of every month
+Last weekday of every month
+Thursday at 1200
+Weekday at 09:30
+Saturday at 4pm
+Saturday at 4:30pm
+Sunday at noon
+Last friday of every month at 8am
+16th of every month
+4th of every month at 4pm
+
+You can test this module by running it with the string after the file name (you can use spaces)
+or without any arguments in which case it will prompt you for an input.
+
+
+Readme for developers/maintainers/people that want to use the code
+==================================================================
+
+parse() cleans up the input searches through the pipes for a valid handler
+once a handler is found we are given a base generator and a set of filter functions
+
+You end up with a pipeline of the following:
+
+f1 . f2 . fx . g
+
+or
+
+f1(f2(fx(g)))
+
+Where f is a filter/applier function and g is the generator. Filters will discard any values
+not meeting their criteria while appliers will alter values passing through them. In principle
+the signature for each is identical, the only difference lies in their use/purpose.
+
+-- To add a new handler --
+Add a new entry to pipes, one of the first entries has extra comments to help.
+
+-- Todo --
+I'm pretty sure the pipes regex search could be improved if it were an actual parser.
+Add support for relative times such as "this time every week"
+
+
+Written by Tefion Jordan
