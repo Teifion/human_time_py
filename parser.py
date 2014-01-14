277046820efa581afabd802acb6293bc31425da3
Igor Vasilcovsky <vasilcovsky@gmail.com>
2014-01-14 08:08:52 -0500

    added comments

--- parser.py
+++ parser.py
@@ -12,61 +12,87 @@ mapping = namedtuple('Mapping', 'pattern, filters')
 pipes = defaultdict(lambda: [])
 
 
-def add_pipe(regexp_pattern, filters, priority):
-    item = mapping(regexp_pattern, filters)
+def add_pipe(regex, filters, priority):
+    """Registers new pipe.
+
+    Register new pipe in internal structure.
+
+    @param regex: regular expression that will trigger filters
+    @param filters: collection of filter generators
+    @param priority: used to sort execution order of filters
+    """
+    item = mapping(regex, filters)
     pipes[priority].append(item)
 
 
-def _compile_format(s, *args):
-    return re.compile(s.format(*args))
 
+# helper functions for
+# >>> _("regexp{}", params)
+# instead of
+# >>> re.compile("regexp{}".format(params))
+_compile_format = lambda s, *args: re.compile(s.format(*args))
 _ = _compile_format
 
 
-# A matching regex
-# a list of functions to compose
-
+# every other Monday
 add_pipe(
     _("other (?P<principle>{})", consts.re_all_day_names),
     [filters._filter_everyother, filters._filter_weekday],
     0
 )
 
+# first monday after second sunday of month
 add_pipe(
     _("(?P<selector>{}) (?P<principle>{}) after (?P<selector2>{}) (?P<principle2>{}) of month", consts.re_all_selector_names, consts.re_day_names, consts.re_all_selector_names, consts.re_day_names),
     [filters._filter_identifier_in_month_after],
     0
 )
 
+# second Monday of month
 add_pipe(
     _("(?P<selector>{}) (?P<principle>{}) of month", consts.re_all_selector_names, consts.re_day_names),
     [filters._filter_identifier_in_month],
     0
 )
 
+# 1st of month
 add_pipe(
     re.compile("(?P<selector>[0-9]{1,2})(?:st|nd|rd|th)? of (?P<principle>month)"),
     [filters._filter_day_number_in_month],
     0
 )
 
+# end of month
 add_pipe(
     re.compile("end of month"),
     [filters._filter_end_of_month],
     0
 )
 
+# friday
 add_pipe(
     _("(?P<principle>{})", consts.re_all_day_names),
     [filters._filter_weekday],
     0
 )
 
-add_pipe(_("at (?P<applicant>{})", consts.re_all_time_names), [filters._apply_time], 1)
-add_pipe(_("^((?!at).)*$", consts.re_all_time_names), [filters._cut_time], 1)
+# apply time if string contains "at <time_pattern>"
+add_pipe(
+    _("at (?P<applicant>{})",  consts.re_all_time_names),
+    [filters._apply_time],
+    1
+)
+
+# cut time if string doesnt contain time pattern
+add_pipe(
+    _("^((?!at (?P<applicant>{})).)*$", consts.re_all_time_names),
+    [filters._cut_time],
+    1
+)
+
+
 """Looks in the pipes list and selects a filter list and generator"""
 def _find_pipes(item):
-    rfunc = generators._generator_day
     filter_functions = []
 
     for priority, mapping in pipes.iteritems():
@@ -76,10 +102,8 @@ def _find_pipes(item):
                 filter_functions.extend([filter_(result) for filter_ in filters])
                 break
 
-
     if filter_functions:
-        #filter_functions.reverse()
-        return consts.compose(*filter_functions), rfunc
+        return consts.compose(*filter_functions), generators._generator_day
 
     raise Exception("Unable to find pipe for item of '{}'".format(item))
 
